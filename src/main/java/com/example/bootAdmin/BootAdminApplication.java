b82ce036b91f57013d9593d81a3eccceb6550d42
nicePow <lbyybl@naver.com>
2025-01-16 17:56:33 +0900

    Update BootAdminApplication.java

--- src/main/java/com/example/bootAdmin/BootAdminApplication.java
+++ src/main/java/com/example/bootAdmin/BootAdminApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAdminServer // 기동시 해당 서버를 Boot Admin Server로 띄우기 위한 설정을 하는 어노테이션이다.rde
+@EnableAdminServer // 기동시 해당 서버를 Boot Admin Server로 띄우기 위한 설정을 하는 어노테이션이다.
 public class BootAdminApplication {
 
     public static void main(String[] args) {
